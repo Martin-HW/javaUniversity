78e9926e099c5740272982861f9e928ca38c7ba3
martin_suarez <martin_suarez_mendez@outlook.com>
2022-09-10 16:53:19 -0500

    first day

---
+++ folder-11/11-02-PalabraThis-CFJ/HolaMundoJava/src/palabrathis/PalabraThis.java
@@ -0,0 +1,20 @@
+package palabrathis;
+
+public class PalabraThis {
+    public static void main(String[] args) {
+        Persona persona = new Persona("Juan", "Perez");
+        System.out.println("persona = " + persona);
+        System.out.println("persona nombre:" + persona.nombre);
+        System.out.println("persona nombre:" + persona.apellido);
+    }
+}
+
+class Persona{
+    String nombre;
+    String apellido;
+    
+    Persona(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
